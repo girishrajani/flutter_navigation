04d7d94c702d02eb54bb7637f89c4e37c4bdfa6c
Girish Rajani <girish.r.rajani@gmail.com>
2021-01-19 12:37:04 +0530

    Home Screen Designed

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,52 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+void main() {
+  runApp(HomeScreen());
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          color: Colors.pink,
+          child: Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                RaisedButton(
+                  padding: EdgeInsets.all(16.0),
+                  child: Text('Second Screen'),
+                  onPressed: () {},
+                  color: Colors.blue,
+                ),
+                SizedBox(
+                  width: 30,
+                ),
+                RaisedButton(
+                  onPressed: () {},
+                  padding: EdgeInsets.all(16.0),
+                  color: Colors.blue,
+                  child: Text(
+                    'Third Screen',
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        appBar: AppBar(
+          backgroundColor: Colors.pink,
+          title: Text('Home Screen'),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.home),
+              onPressed: () {},
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
